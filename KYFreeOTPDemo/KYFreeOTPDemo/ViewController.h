c4d030df36e15e6058e9970ae5347f33119af3ba
kingly <libintm@163.com>
2017-11-24 17:01:33 +0800

    添加 OTP的demo

---
+++ KYFreeOTPDemo/KYFreeOTPDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KYFreeOTPDemo
+//
+//  Created by kingly on 2017/11/24.
+//  Copyright © 2017年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
