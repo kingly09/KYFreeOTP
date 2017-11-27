2eddc757e1a4f65d62afa4c40be4565f21c64576
kingly <libintm@163.com>
2017-11-27 12:35:27 +0800

    addToken

--- KYFreeOTP/KYFreeOTP.h
+++ KYFreeOTP/KYFreeOTP.h
@@ -16,6 +16,30 @@ extern NSString *const KYFreeOTPSHAMD5Algorithm;
 extern NSString *const KYFreeOTPTokenTypeTotp; //totp算法，时间同步
 extern NSString *const KYFreeOTPTokenTypeHotp; //hotp算法，事件同步
 
+
+@interface KYOTPModel : NSObject
+
+@property (nonatomic,copy) NSString* issuer;     //颁发者
+@property (nonatomic,copy) NSString* uid;        //用户ID
+@property (nonatomic,copy) NSString *secret;     //密钥（base32加密字符串）
+
+@property (nonatomic,copy) NSString* algorithm;  //加密方式
+@property (nonatomic,copy) NSString* tokenType;  //算法方式
+
+@property (nonatomic, assign) NSInteger digits;  //otp的位数
+@property (nonatomic, assign) NSInteger interval; //时间间隔（秒）
+
+@end
+
+
 @interface KYFreeOTP : NSObject
+
+
+/**
+ 添加addToken
+
+ @param OTPModel otp对象
+ */
+-(void)addToken:(KYOTPModel *)OTPModel;
 
 @end
--- KYFreeOTP/KYFreeOTP.m
+++ KYFreeOTP/KYFreeOTP.m
@@ -7,16 +7,57 @@
 //
 
 #import "KYFreeOTP.h"
+#import "TokenStore.h"
 
-NSString *const KYFreeOTPSHA1Algorithm = @"SHA1";
-NSString *const KYFreeOTPSHA256Algorithm = @"SHA256";
-NSString *const KYFreeOTPSHA512Algorithm = @"SHA512";
-NSString *const KYFreeOTPSHAMD5Algorithm = @"MD5";
+NSString *const KYFreeOTPSHA1Algorithm = @"sha1";
+NSString *const KYFreeOTPSHA256Algorithm = @"sha256";
+NSString *const KYFreeOTPSHA512Algorithm = @"sha512";
+NSString *const KYFreeOTPSHAMD5Algorithm = @"md5";
 
 
 NSString *const KYFreeOTPTokenTypeTotp = @"totp";
 NSString *const KYFreeOTPTokenTypeHotp = @"hotp";
 
+@implementation KYOTPModel
+
+@end
+
+
 @implementation KYFreeOTP
+
+/**
+ 添加addToken
+ 
+ @param OTPModel otp对象
+ */
+-(void)addToken:(KYOTPModel *)OTPModel{
+    
+    // Get algorithm
+    const char *algo = "sha1";       //sha1 算法
+    if ( OTPModel.algorithm == KYFreeOTPSHA1Algorithm) {
+        algo = "sha1";
+    }else if ( OTPModel.algorithm == KYFreeOTPSHA256Algorithm) {
+        algo = "sha256";
+    }else if ( OTPModel.algorithm == KYFreeOTPSHA512Algorithm) {
+        algo = "sha512";
+    }else if ( OTPModel.algorithm == KYFreeOTPSHAMD5Algorithm) {
+        algo = "md5";
+    }
+ 
+    // Built URI
+    NSURLComponents* urlc = [[NSURLComponents alloc] init];
+    urlc.scheme = @"otpauth";
+    urlc.host = [OTPModel.tokenType isEqualToString:KYFreeOTPTokenTypeTotp] ? KYFreeOTPTokenTypeTotp : KYFreeOTPTokenTypeHotp;
+    urlc.path = [NSString stringWithFormat:@"/%@:%@", OTPModel.issuer, OTPModel.uid];
+    urlc.query = [NSString stringWithFormat:@"algorithm=%s&digits=%lu&secret=%@&%s=%lu",
+                  algo, (long)OTPModel.digits, OTPModel.secret,
+                  [OTPModel.tokenType isEqualToString:KYFreeOTPTokenTypeTotp]?"period" : "counter",OTPModel.interval];
+
+    // Make token
+    Token* token = [[Token alloc] initWithURL:[urlc URL]];
+    if (token != nil)
+        [[[TokenStore alloc] init] add:token];
+    
+}
 
 @end
